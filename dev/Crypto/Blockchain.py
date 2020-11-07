cb1467b33ce30a655e336b9c574c2e7ae5b9b036
Gokul R Krishnan <63335983+gokulkrishnxn@users.noreply.github.com>
2020-11-07 21:01:07 +0530

    Add files via upload

---
+++ dev/Crypto/Blockchain.py
@@ -0,0 +1,39 @@
+class Block:
+  
+    def __init__():
+    	pass
+
+
+    def calculate_hash():
+    	pass
+
+
+    	
+
+class Blockchain:
+
+	def __init__(self):
+		pass
+
+	def construct_genesis(self):
+		pass
+
+	def construct_block(self, proof_no, prev_hash):
+		pass
+
+
+ @staticmethod
+    def check_validity():
+    	pass
+
+    def new_data(self, sender, recipient, quantity):
+    	pass
+
+ @staticmethod
+    def construct_proof_of_work(prev_proof):
+    	pass
+
+ @property
+    def last_block(self):
+ 	return self.chain[-1]
+ 
---
+++ dev/Game/Main.java
@@ -0,0 +1,8 @@
+import java.awt.Dimension;
+import java.awt.image;
+import java.awt.Toolkit;
+import javax.swing.*;
+
+public class TopClass {
+	
+}
